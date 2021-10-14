6b52c7ea8b8846cc485582e2c9cc02e62fd4644d
mehmeetyuksel <67976021+mehmeetyuksel@users.noreply.github.com>
2021-10-14 19:20:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,39 +23,4 @@ function currentSlide(n) {
   }
 
  
-  
-/*   const url = 'https://api.openweathermap.org/data/2.5/';
-  const key  =  'fae7c3f1050db7d484602d10705634f9';
-  const sehir ='london';
- 
-  const getResult = (sehir) => {
-  let query = `${url}weather?q=${sehir}&cnt=7&appid=${key}&units=metric&lang=tr`
-  fetch(query).then(weather => {
-    return weather.json()
-  })
-  .then(displayResult)
-}
-  
-const displayResult = (result) => {
-  console.log(result)
 
-}
-
-getResult (sehir); */
- 
-
-const url = `http://api.weatherapi.com/v1/`;
-const key = `28193ed4b743490692a92524212408`;
-const sehir ='London';
-
-const getResult = (sehir) => {
-let query = `${url}forecast.json?key=${key}&q=${sehir}&days=7`
-fetch(query).then(weather => {
-  return weather.json()
-})
-.then(displayResult)
-}
- const displayResult = (result) => {
-document.getElementById("first").textContent += result.location.name
-console.log(result)}
-getResult(sehir);
